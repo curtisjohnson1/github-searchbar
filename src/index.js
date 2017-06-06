e84346617485323e20dde0acb84ce890d19311e9
curtis johnson <curtisianjohnson@gmail.com>
2017-06-06 19:34:51 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import thunk from 'redux-thunk';
+
+
+import App from './Components/App';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
