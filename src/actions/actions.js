cd29d4cefe523356c017dd5c9072093969cd5192
curtis johnson <curtisianjohnson@gmail.com>
2017-06-09 11:57:03 +0100

    actions fully working

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -5,21 +5,21 @@ import * as types from './types';
 export function fetchRepoRequest () {
     return {
         type: types.FETCH_REPO_REQUEST
-    }
+    };
 }
 
-export function fetchRepoSuccess (repo) {
+export function fetchRepoSuccess (data) {
     return {
         type: types.FETCH_REPO_SUCCESS,
-        repo
-    }
+        data
+    };
 }
 
 export function fetchRepoError (error) {
     return {
         type: types.FETCH_REPO_ERROR,
         error
-    }
+    };
 }
 
 export function fetchRepo (wordSearch) {
@@ -33,6 +33,41 @@ export function fetchRepo (wordSearch) {
                 })
                 .catch (error => {
                     dispatch (fetchRepoError (error));
+                });
+    };
+}
+
+export function fetchUserRepoRequest () {
+    return {
+        type: types.FETCH_USER_REPO_REQUEST
+    };
+}
+
+export function fetchUserRepoSuccess (data) {
+    return {
+        type: types.FETCH_USER_REPO_SUCCESS,
+        data
+    };
+}
+
+export function fetchUserRepoError (error) {
+    return {
+        type: types.FETCH_USER_REPO_ERROR,
+        error
+    };
+}
+
+export function fetchUserRepo (userRepo, repoName) {
+
+    return dispatch => {
+        dispatch (fetchUserRepoRequest ());
+            axios
+                .get (`https://api.github.com/repos/${userRepo}/${repoName}`)
+                .then (res => {
+                    dispatch (fetchUserRepoSuccess (res.data));
+                })
+                .catch (error => {
+                    dispatch (fetchUserRepoError (error));
                 });
     };
 }
