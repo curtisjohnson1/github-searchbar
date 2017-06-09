9255f0126b354c9750a9a63e7447e14d453ac11c
curtis johnson <curtisianjohnson@gmail.com>
2017-06-09 12:01:34 +0100

    react-router fully working

--- src/index.js
+++ src/index.js
@@ -6,9 +6,22 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import thunk from 'redux-thunk';
 
 
-import App from './Components/App';
+import App from './components/App';
+import FrontPage from './components/FrontPage';
+import Repo from './components/Repo';
+import Reducer from './reducers/reducer';
+import './css/main.css';
+
+const store = createStore(Reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('app')
+    <Provider store={store}>
+        <Router history={browserHistory}>    
+            <Route path='/' component={App}> 
+                <IndexRoute component={FrontPage} />   
+                <Route path='/repos/:user/:repo' component={Repo} />
+            </Route>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
 );
